1d1ef5c06c4e2401a37482ca3227fd2f088ec925
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-08-31 13:35:46 +0300

    Hydraulic Req

--- rest-api/requests/insertHidrolik.js
+++ rest-api/requests/insertHidrolik.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const path = require("path");
 
 const connection = mysql.createConnection({
     host: process.env.DB_SERVER,
@@ -11,10 +12,13 @@ function insertHidrolik(req, res) {
     const { OrderNumber, OrderDate, Type, InCharge, PDF, PartList } = req.body;
     const Siparis_Tarihi = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
+    const PDF_URL = path.join(__dirname, 'data', 'hydraulicUnits', PDF);
+    const Excel_URL = path.join(__dirname, 'data', 'partList', PartList);
+
     const insertQuery = `INSERT INTO HidrolikUnit (unitID, UserName, Siparis_Numarasi, Siparis_Tarihi, Unite_Tipi, Pdf_File, Excel_File, Created_By)
                          VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    connection.query(insertQuery, [OrderNumber, InCharge, OrderNumber, Siparis_Tarihi, Type, PDF, PartList, InCharge], (err, results) => {
+    connection.query(insertQuery, [OrderNumber, InCharge, OrderNumber, Siparis_Tarihi, Type, PDF_URL, Excel_URL, InCharge], (err, results) => {
         if (err) {
             console.error('Sorgu hatası:', err);
             res.status(500).json({ error: 'Sunucu hatası' });
