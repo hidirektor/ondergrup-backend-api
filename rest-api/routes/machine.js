518eecb1e5a8fbe33f7691fe303dae708cf758d3
Halil İbrahim Direktör <hidirektor@gmail.com>
2023-10-13 16:01:11 +0300

    Bug Fix

--- rest-api/routes/machine.js
+++ rest-api/routes/machine.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const machineController = require('../controllers/machineController')
 
 router.post('/add', machineController.addMachine)
-router.post('/insertMachineData', machineController.insertMachineData)
 router.post('/updateMachineData', machineController.updateMachineData)
 router.post('/getMachines', machineController.getMachines)
 
