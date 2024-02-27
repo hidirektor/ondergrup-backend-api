390fc8d5f90cbff5bde1f9ec2cd54aa0f019b903
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-02-27 07:39:07 +0300

    Machine Errors

--- rest-api/routes/user.js
+++ rest-api/routes/user.js
@@ -7,6 +7,6 @@ router.post('/updateProfile', userController.updateProfile)
 router.post('/updatePass', userController.updatePass)
 router.post('/getAllUsers', userController.getAllUsers)
 router.post('/deleteUser', userController.deleteUser)
-router.post('updateRole', userController.updateRole)
+router.post('/updateRole', userController.updateRole)
 
 module.exports = router;
