084ddca00811886ebda67def9e0d1b2a7eaa9568
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-08-05 14:43:12 +0300

    DeActivate Sub User

---
+++ controllers/subuser/deActivateSubUser.js
@@ -0,0 +1,93 @@
+const SubUser = require('../../models/SubUser');
+const User = require('../../models/User');
+const ActionLog = require("../../models/ActionLog");
+
+/**
+ * @swagger
+ * /deActivateSubUser:
+ *   post:
+ *     summary: DeActivate a subuser and associated user by ID
+ *     tags: [Sub User Management]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 description: The ID of the subuser to delete
+ *                 example: "subuser123"
+ *     responses:
+ *       200:
+ *         description: SubUser deactivated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "SubUser deactivated successfully"
+ *       400:
+ *         description: Invalid request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "id is required"
+ *       404:
+ *         description: SubUser or User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "SubUser not found"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Internal server error"
+ */
+
+module.exports = async (req, res) => {
+    try {
+        const { subUserID } = req.body;
+
+        if (!subUserID) {
+            return res.status(400).json({ message: 'subUserID is required' });
+        }
+
+        const subUser = await SubUser.findOne({ where: { userID: subUserID } });
+        if (!subUser) {
+            return res.status(404).json({ message: 'SubUser not found' });
+        }
+
+        const user = await User.findOne({ where: { userID: subUser.userID } });
+        const userID = user.userID;
+        const userName = user.userName;
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.isActive = false;
+        user.save();
+
+        res.status(200).json({ message: 'SubUser deactivated successfully' });
+    } catch (error) {
+        console.error('Error deactivating SubUser:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
--- routes/subuser.js
+++ routes/subuser.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const createSubUser = require('../controllers/subuser/createSubUser');
 const deleteSubUser = require('../controllers/subuser/deleteSubUser');
+const deActivateSubUser = require('../controllers/subuser/deActivateSubUser');
 const editSubUser = require('../controllers/subuser/editSubUser');
 const getSubUsers = require('../controllers/subuser/getSubUsers');
 
@@ -11,6 +12,7 @@ const roleMiddleware = require('../middleware/roleMiddleware');
 
 router.post('/createSubUser', authMiddleware, createSubUser);
 router.post('/deleteSubUser', authMiddleware, deleteSubUser);
+router.post('/deActivateSubUser', authMiddleware, deActivateSubUser);
 router.post('/editSubUser', authMiddleware, editSubUser);
 router.post('/getSubUsers', authMiddleware, getSubUsers);
 
