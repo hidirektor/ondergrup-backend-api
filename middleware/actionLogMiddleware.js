d50ef0e922dc3f22d213c1020ef1debda7b9d7c6
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-08-19 01:02:16 +0300

    ActionLog Update

--- middleware/actionLogMiddleware.js
+++ middleware/actionLogMiddleware.js
@@ -8,21 +8,21 @@ const actionLogMiddleware = (operationType, operationName) => {
 
             let affectedUser = null, affectedUserIDData = null,  sourceUser = null, affectedNameSurnameData = null, affectedUserNameData = null, sourceNameSurnameData = null;
 
-            if(sourceUserID) {
+            if(sourceUserID && !sourceUserID.isEmpty()) {
                 sourceUser = await Users.findOne({ where: { userID: sourceUserID }});
                 if(sourceUser) {
                     sourceNameSurnameData = sourceUser.nameSurname;
                 }
             }
 
-            if(affectedUserID) {
+            if(affectedUserID && !affectedUserID.isEmpty()) {
                 affectedUserIDData = affectedUserID;
                 affectedUser = await Users.findOne({ where: { userID: affectedUserID } });
                 if(affectedUser) {
                     affectedNameSurnameData = affectedUser.nameSurname;
                 }
             } else {
-                if(affectedUserName) {
+                if(affectedUserName && !affectedUserName.isEmpty()) {
                     affectedUser = await Users.findOne({ where: { userName: affectedUserName } });
                     if(affectedUser) {
                         affectedUserIDData = affectedUser.userID;
