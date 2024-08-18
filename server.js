e1ee5c01adb649e8368a849f7566526e04daa54f
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-08-19 01:41:25 +0300

    body limit increased

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
@@ -26,6 +27,9 @@ const swaggerSpecs = require('./helpers/swaggerOptions');
 fs.writeFileSync('./swagger.json', JSON.stringify(swaggerSpecs, null, 2));
 
 app.use(cors());
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 app.use('/api/v2/auth', authRoutes);
